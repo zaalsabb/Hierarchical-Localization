94f4dc5d511939612f9a8d52cfd1e8d90acd259e
zaid <zaalsabb@uwaterloo.ca>
2021-12-31 16:51:52 -0500

    added search_images.py and image_retrieval.py

--- hloc/image_retrieval.py
+++ hloc/image_retrieval.py
@@ -12,47 +12,13 @@ from .utils.read_write_model import read_images_binary
 from .utils.io import list_h5_names
 
 
-def parse_names(prefix, names, names_all):
-    if prefix is not None:
-        if not isinstance(prefix, str):
-            prefix = tuple(prefix)
-        names = [n for n in names_all if n.startswith(prefix)]
-    elif names is not None and isinstance(names, (str, Path)):
-        names = parse_image_lists(names)
-    elif names is not None and isinstance(names, collections.Iterable):
-        names = list(names)
-    else:
-        raise ValueError('Provide either prefixes of names, a list of '
-                         'images, or a path to list file.')
-    return names
-
-
 def main(descriptors, output, num_matched,
-         query_prefix=None, query_list=None,
-         db_prefix=None, db_list=None, db_model=None, db_descriptors=None, images=None):
+         images, query, db_descriptors=None):
     logging.info('Extracting image pairs from a retrieval database.')
 
-    # We handle multiple reference feature files.
-    # We only assume that names are unique among them and map names to files.
-    if db_descriptors is None:
-        db_descriptors = descriptors
-    if isinstance(db_descriptors, (Path, str)):
-        db_descriptors = [db_descriptors]
-    name2db = {n: i for i, p in enumerate(db_descriptors)
-               for n in list_h5_names(p)}
-    db_names_h5 = list(name2db.keys())
-    query_names_h5 = list_h5_names(descriptors)
-
-    if db_model:
-        images = read_images_binary(db_model / 'images.bin')
-        db_names = [i.name for i in images.values()]
-    else:
-        db_names = parse_names(db_prefix, db_list, db_names_h5)
-    if len(db_names) == 0:
-        raise ValueError('Could not find any database image.')
-    query_names = parse_names(query_prefix, query_list, query_names_h5)
-
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    db_names = list_h5_names(db_descriptors)
+    query_names = list_h5_names(descriptors)
 
     def get_descriptors(names, path, name2idx=None, key='global_descriptor'):
         if name2idx is None:
@@ -65,23 +31,24 @@ def main(descriptors, output, num_matched,
                     desc.append(fd[n][key].__array__())
         return torch.from_numpy(np.stack(desc, 0)).to(device).float()
 
-    db_desc = get_descriptors(db_names, db_descriptors, name2db)
+    db_desc = get_descriptors(db_names, db_descriptors)
     query_desc = get_descriptors(query_names, descriptors)
     sim = torch.einsum('id,jd->ij', query_desc, db_desc)
     topk = torch.topk(sim, num_matched, dim=1).indices.cpu().numpy()
 
     pairs = []
-    try:
-        mkdir(str(output.parent)+'/matches')
-    except OSError as error:
-        print(error)
+
+    shutil.rmtree(str(output.parent)+'/matches',ignore_errors=True)
+    mkdir(str(output.parent)+'/matches')
 
     for query, indices in zip(query_names, topk):
+        query_match_folder = str(output.parent)+'/matches/'+query.split('.')[0]
+        mkdir(query_match_folder)
         for i in indices:
             pair = (query, db_names[i])
             pairs.append(pair)
             if images is not None:
-                shutil.copy(path.join(str(images), db_names[i]), path.join(str(output.parent), 'matches', db_names[i]))
+                shutil.copy(path.join(str(images), db_names[i]), path.join(query_match_folder, db_names[i]))
 
     logging.info(f'Found {len(pairs)} pairs.')
     with open(output, 'w') as f:
---
+++ search_images.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from hloc import extract_features, image_retrieval, match_features
+import os
+import argparse
+
+def main(dataset,outputs, num_matches, build_database):
+    dataset = Path(dataset)
+    images = dataset / 'images/'
+    query = dataset / 'query/'
+
+    outputs = Path(outputs)
+    loc_pairs = outputs / f'pairs-query-netvlad{num_matches}.txt'  # top-k retrieved by NetVLAD
+    features_path_query = outputs / 'global-feats-query.h5'
+
+    # pick one of the configurations for extraction and matching
+    retrieval_conf = extract_features.confs['netvlad']
+
+    features_path = Path.with_suffix(outputs / retrieval_conf['output'],'.h5')
+    ## If previous descriptors file exists, delete it ##
+    if build_database:
+        if os.path.isfile(features_path):
+            os.remove(features_path)
+        global_descriptors = extract_features.main(retrieval_conf, images, outputs)
+    else:
+        global_descriptors = Path(features_path)
+
+    ## If previous query descriptors file exists, delete it ##
+    if os.path.isfile(features_path_query):
+        os.remove(features_path_query)
+
+    global_descriptors_q = extract_features.main(retrieval_conf, query, outputs,
+                                                feature_path=features_path_query)
+
+    image_retrieval.main(
+        global_descriptors_q, loc_pairs, num_matches,
+        images, query, db_descriptors=global_descriptors)
+
+    # feature_conf = extract_features.confs['superpoint_max']
+    # matcher_conf = match_features.confs['superglue']
+    # features = extract_features.main(feature_conf, images, outputs)
+    # loc_matches = match_features.main(
+    #     matcher_conf, loc_pairs, feature_conf['output'], outputs)        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True)
+    parser.add_argument('--outputs', type=str, required=True)
+    parser.add_argument('--num_matches', type=int, required=True)
+    parser.add_argument('--build_database', default=False, action='store_true')
+    args = parser.parse_args()
+    main(**args.__dict__)
